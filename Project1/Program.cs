e5ea568a0db4a9d54f75e4ffacf095c03dfa1b08
Maria Skyvova <maria.nedelova@gmail.com>
2025-04-10 20:36:21 +0200

    Vysvetlenie statickej metody

--- Project1/Program.cs
+++ Project1/Program.cs
@@ -1,12 +1,36 @@
 ﻿namespace HelloWorld;
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World! 😎");
-    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        car car = new car("white");
+        car.SetColor("blue");
+
+        int rectangleArea = Rectangle.Calculate(8, 9);
+        Console.WriteLine(rectangleArea);
+    }
+}
+//
+class car
+{
+    private string color;
+    public car(string color)
+    {
+        this.color = color;
+    }
+    public void SetColor(string color)
+    {
+        this.color = color;
+    }
 }
 
-class Auto {
-    
+
+// Method that calculates area of rectangle without an instance of class Rectangle
+class Rectangle
+{
+    public static int Calculate(int a, int b)
+    {
+        return a * b;
+    }
 }
